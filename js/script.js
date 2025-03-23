0e32bb93b0edcdb54a109e1b75c4da23706ca2ef
default <trisha.ray@stu.ocsb.ca>
2025-03-23 16:45:47 +0000

    js

--- js/script.js
+++ js/script.js
@@ -1,12 +1,19 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
+// Created by: Trisha Ray
 // Created on: Sep 2020
 // This file contains the JS functions for index.html
 
-/**
- * Displays an alert with the message "Hello, World!"
- */
-function myButtonClicked() {
-  alert("Hello, World!")
+"use strict"
+
+function calculateAreaOfParallelogram () {
+  // input
+  const baselength = parseInt(document.getElementById('base-length').value)
+  const heightlength = parseInt(document.getElementById('height-length').value)
+
+  // process
+  const areaOfParallelogram = baselength * heightlength
+
+  // output
+  document.getElementById('area').innerHTML = 'Area of parallelogram is: ' + areaOfParallelogram + ' cm²'
 }
